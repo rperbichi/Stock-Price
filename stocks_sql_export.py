985c5a9c1a90111812cd09f0cb23af7d1f75ba08
Raul Perbichi <rperbichi97@gmail.com>
2025-03-07 23:28:20 -0700

    Dataset being added to a SQL database

--- stocks_sql_export.py
+++ stocks_sql_export.py
@@ -2,7 +2,25 @@ import sqlite3
 import pandas as pd
 import kagglehub
 
-# Dowloading data
-path = kagglehub.dataset_download("mczielinski/bitcoin-historical-data")
-print("Path to dataset files:", path)
+# Download latest version
+path = kagglehub.dataset_download("andrewmvd/sp-500-stocks")
+# print("Path to dataset files:", path) 
 
+csv_file = "sp500_companies.csv"
+df = pd.read_csv(csv_file)
+
+# Deleting exchange column
+df.drop(columns=["Exchange"], inplace=True)
+
+# Handling errors
+df["Marketcap"] = pd.to_numeric(df["Marketcap"], errors="coerce")
+df["Ebitda"] = pd.to_numeric(df["Ebitda"], errors="coerce")
+df["Revenuegrowth"] = pd.to_numeric(df["Revenuegrowth"], errors="coerce")
+
+conn = sqlite3.connect("sp500_companies.db")
+df.to_sql("companies", conn, if_exists="replace", index=False)
+
+# Export cleaned data as CSV for Power BI
+df.to_csv("sp500_companies_cleaned.csv", index=False)
+
+conn.close()
